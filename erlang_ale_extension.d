f2a671424b1036f089636412f70b0602d725f74e
Robert Balogh <ethrbh@gmail.com>
2015-09-09 11:46:25 +0200

    Add auto generated by erlang-mk dependency file

---
+++ erlang_ale_extension.d
@@ -0,0 +1,3 @@
+src/ale_handler.erl:: include/ale_common.hrl include/ale_type_def.hrl; @touch $@
+src/mcp23x17.erl:: include/ale_common.hrl include/ale_type_def.hrl include/mcp23x17.hrl; @touch $@
+src/mcp7940n.erl:: include/ale_common.hrl include/mcp7940n.hrl; @touch $@
